0dda0865d39c7dc5804f064dd95ee9a49e3ce05f
JATT98 <andrewstarott@gmail.com>
2021-10-23 06:03:36 -0600

    a

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -9,10 +9,48 @@ const pool = new Pool({
 });
 
 const getUsers = async (req, res) => {
-    const response = await pool.query('SELECT * FROM ut_cuenta ORDER BY id ASC');
+    const response = await pool.query('SELECT * FROM ut_rol ORDER BY nombre ASC');
     res.status(200).json(response.rows);
 };
 
+const getUserInfo = async (req, res) => {
+    const {name, pass} = req.body;
+    const response = await pool.query('SELECT * FROM f_login_usuario($1,$2)',[name, pass]);
+    res.json(response.rows);
+};
+
+const getProducts = async (req, res) => {
+    const response = await pool.query('SELECT * FROM pt_descripcion');
+    res.status(200).json(response.rows);
+};
+
+const getProductsByCategory = async (req, res) => {
+    const id = req.params.id;
+    const response = await pool.query('SELECT pr.* FROM pt_descripcion AS pr '
+                                        + 'INNER JOIN pt_relacion_categoria AS rel '
+                                        + 'ON rel.prc_producto = pr.pc_producto '
+                                        + 'INNER JOIN pt_categoria AS cat '
+                                        + 'ON cat.pc_categoria = rel.prc_categoria '
+                                        + 'WHERE rel.prc_categoria = $1 OR '
+                                        + 'cat.rc_categoria_padre = $2',[id, id]);
+    res.json(response.rows);
+};
+
+const postOrderDetail = async (req, res) => {
+    const id = req.params.id;
+    const response = await pool.query('SELECT pr.* FROM pt_descripcion AS pr '
+                                        + 'INNER JOIN pt_relacion_categoria AS rel '
+                                        + 'ON rel.prc_producto = pr.pc_producto '
+                                        + 'INNER JOIN pt_categoria AS cat '
+                                        + 'ON cat.pc_categoria = rel.prc_categoria '
+                                        + 'WHERE rel.prc_categoria = $1 OR '
+                                        + 'cat.rc_categoria_padre = $2',[id, id]);
+    res.json(response.rows);
+};
+
 module.exports = {
-    getUsers
+    getUsers,
+    getUserInfo,
+    getProducts,
+    getProductsByCategory
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/index.controller');
+const { getUsers, getUserInfo, getProducts, getProductsByCategory, deleteUser } = require('../controllers/index.controller');
 
 router.get('/users', getUsers);
+router.put('/getUser', getUserInfo);
+router.get('/products', getProducts);
+router.get('/products/:id', getProductsByCategory);
 
 module.exports = router;
